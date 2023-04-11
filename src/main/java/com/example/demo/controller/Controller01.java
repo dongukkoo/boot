a850da0a5beeb6063cd2c385d29ac09f8e863c7d
dongukkoo <123609752+dongukkoo@users.noreply.github.com>
2023-04-12 01:12:41 +0900

    spring boot

---
+++ src/main/java/com/example/demo/controller/Controller01.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class Controller01 {
+
+	@RequestMapping("/sub1/link1")
+	public void method01() {
+		System.out.println("일함!@!@!@!@!@");
+	}
+	
+	@RequestMapping("/sub1/link2")
+	public void method02() {
+		System.out.println("/sub1/link2 경로 요청에서 일하는 메소드!!!!");
+	}
+	
+}
