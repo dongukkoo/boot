7ecddec53fedc2dadf8bc1bf373a338d37a1aad3
dongukkoo <123609752+dongukkoo@users.noreply.github.com>
2023-04-18 17:50:14 +0900

    controller

---
+++ src/main/webapp/WEB-INF/sql/27Update.sql
@@ -0,0 +1,7 @@
+SELECT * FROM Customers ORDER BY 1 DESC;
+INSERT INTO Customers (CustomerID, CustomerName)
+VALUES(92, '강백호');
+
+SELECT * FROM Customers ORDER BY 1 DESC;
+
+SELECT * FROM Employees ORDER BY 1 DESC;
---
+++ src/main/webapp/WEB-INF/sql/28Update.sql
@@ -0,0 +1,22 @@
+INSERT INTO Employees (EmployeeID, LastName, FirstName)
+VALUES (11, '박지성', '두개의심장');
+
+INSERT INTO Employees (LastName, FirstName)
+VALUES('차범근', '차붐');
+
+SELECT * FROM Employees ORDER BY 1 DESC;
+
+DESC Employees;
+DESC Customers;
+
+INSERT INTO Customers(CustomerName, ContactName)
+VALUES ('정대만', 'daeman');
+
+SELECT * FROM Customers ORDER BY 1 DESC;
+
+INSERT INTO Suppliers (SupplierName, City)
+VALUES ('Mike Trout', 'Los Angeles');
+
+SELECT * FROM Suppliers ORDER BY 1 DESC;
+
+
