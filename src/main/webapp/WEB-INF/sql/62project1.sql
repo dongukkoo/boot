7e2debed388e14539329fae3f40e37cedc516405
dongukkoo <123609752+dongukkoo@users.noreply.github.com>
2023-05-08 16:46:07 +0900

    prj

---
+++ src/main/webapp/WEB-INF/sql/62project1.sql
@@ -0,0 +1,11 @@
+USE Board;
+CREATE TABLE Member(
+	id VARCHAR(20) PRIMARY KEY,
+    password VARCHAR(100) NOT NULL,
+    nickName VARCHAR(20) NOT NULL UNIQUE,
+    email VARCHAR(100) UNIQUE,
+    inserted DATETIME DEFAULT NOW()
+);
+
+SELECT * FROM Member;
+SELECT * FROM Board;
